8981fbb14a14d4ab9cd0d4680135f8de40654d74
Ragul <ragul.thangaraju@concertidc.com>
2022-03-14 18:56:48 +0530

    initial

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const cors = require("cors");
 
 app.use(express.static('/'));
 
-const corsOptions ={
-    origin:'http://localhost:3000',
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:3000',
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+app.use(cors());
 
 app.get('/client', (req, res) => {
     return res.status(200).sendFile(`${__dirname}/client.html`);
